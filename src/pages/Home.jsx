209e94d077bc7f8dfb0ad7753bdb3a7473c33e94
zikrya <77081809+zikrya@users.noreply.github.com>
2023-10-07 21:50:47 -0400

    FORGOT TO PUSH

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 const Home = () => {
+    const { currentUser, logout } = useAuth();
+    const navigate = useNavigate();
+    const handleLogout = async () => {
+        try {
+            await logout(navigate("/"));
+        } catch (err) {
+            console.error("Error logging out:", err);
+        }
+    };
     return (
         <>
         <h1>Home Page</h1>
+        {currentUser && <button
+      onClick={handleLogout}
+        type="button"
+        data-ripple-light="true"
+      >
+        <span>logout</span>
+      </button>}
         </>
      );
 }
