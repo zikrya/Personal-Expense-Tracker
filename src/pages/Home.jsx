9fc18c6f69e2705696054870634ce408a10c2916
zikrya <77081809+zikrya@users.noreply.github.com>
2023-10-13 16:32:54 -0400

    [STYLING] Temp styling for homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,67 @@
 import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+
 const Home = () => {
     const { currentUser, logout } = useAuth();
     const navigate = useNavigate();
+
     const handleLogout = async () => {
         try {
-            await logout(navigate("/"));
+            await logout();
+            navigate("/");
         } catch (err) {
             console.error("Error logging out:", err);
         }
     };
+
+    const colors = {
+        paleGreen: "#D3F6DB",
+        green: "#5B8260",
+        darkGreen: "#182825",
+        navy: "#1F2937",
+        lightBlue: "#D6EBFF"
+    };
+
     return (
-        <>
-        <h1>Home Page</h1>
-        {currentUser && <button
-      onClick={handleLogout}
-        type="button"
-        data-ripple-light="true"
-      >
-        <span>logout</span>
-      </button>}
-        </>
-     );
+        <div className="flex items-center justify-between min-h-screen p-5 bg-gray-100" style={{ backgroundColor: colors.paleGreen }}>
+
+            <div className="flex-1 flex flex-col items-start justify-center p-5 space-y-6">
+                <h1 className="text-4xl font-bold" style={{ color: colors.navy }}>Personal Expense Tracker</h1>
+
+                <p className="text-left mb-5" style={{ color: colors.darkGreen }}>
+                    Embark on a journey toward robust financial health with our Personal Expense Tracker - your smart ally in mastering the art of budgeting, expense tracking, and reaching financial goals. Developed with the unique challenges of college students in mind, our app simplifies financial management, turning it from a source of stress into a stepping stone towards a secure, financially stable future. Let's redefine your spending, together - because your financial well-being matters!
+                </p>
+
+                {currentUser ? (
+                    <button
+                        onClick={handleLogout}
+                        className="bg-green-600 text-white px-4 py-2 rounded shadow hover:bg-green-700 transition duration-300 ease-in-out"
+                        style={{ backgroundColor: colors.green, color: colors.lightBlue }}
+                    >
+                        Sign Out
+                    </button>
+                ) : (
+                    <button
+                        onClick={() => navigate("/register")}
+                        className="bg-green-600 text-white px-4 py-2 rounded shadow hover:bg-green-700 transition duration-300 ease-in-out"
+                        style={{ backgroundColor: colors.green, color: colors.lightBlue }}
+                    >
+                        Join Now
+                    </button>
+                )}
+            </div>
+
+            {/* Placeholder for the illustration or any other content on the right */}
+            <div className="flex-1">
+            <img
+                  src="Cifrão_symbol.svg.png"
+                  className="h-56 w-full object-contain lg:h-auto lg:rounded-lg"
+                />
+            </div>
+
+        </div>
+    );
 }
 
 export default Home;
+
