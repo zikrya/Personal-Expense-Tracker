d75f397cb0d8e51ce1c0059e32d3afe8236a6fc3
Siema Alam <siemaalam@gmail.com>
2023-10-15 17:02:55 -0400

    Add dashboard cards to dashboard page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mint: '#D3F6DB',
+        green: '#5b8266',
+        darkgreen: '#182825',
+        darkblue: '#1F2937',
+        lightblue: '#D6EBFF'
+      },
+    },
   },
   plugins: [],
 }
