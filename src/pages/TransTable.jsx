a383bebdd7af171c8a3f5b2e072924370f44dbd9
Noel Mathew <noelsmathew@gmail.com>
2023-10-15 18:28:20 -0400

    added cards to dashboard page

--- src/pages/TransTable.jsx
+++ src/pages/TransTable.jsx
@@ -24,38 +24,72 @@ const TransTable = () => {
         }
     };
     return (
-    
-    <div className="p-8 w-full overflow-x-auto">
-      <div className="mb-4 text-2xl font-semibold text-gray-800">Recent Activity</div>
-      <div className="grid grid-cols-4">
-      <table className="w-full col-span-3 border-collapse table-auto bg-white rounded-lg shadow">
-        <thead>
-          <tr>
-            <th className="border p-3 bg-gray-100 text-left">Date</th>
-            <th className="border p-3 bg-gray-100 text-left">Category</th>
-            <th className="border p-3 bg-gray-100 text-right">Amount</th>
-          </tr>
-        </thead>
-      <tbody>
-        {transactions.map((transaction, index) => (
-          <tr
-            key={index}
-            className="transition-colors hover:bg-gray-50"
-          >
-            <td className="border p-3">{transaction.date}</td>
-            <td className="border p-3">{transaction.description}</td>
-            <td className="border p-3 text-green-600 font-semibold text-right">
-              ${transaction.amount.toFixed(2)}
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-    <button className="ml-4 px-4 py-2 rounded-md bg-emerald-200 text-white hover:bg-emerald-300">
-          Add Transaction
-        </button>
-  </div>
-  </div>
+        <><div className="px-8 py-6 grid grid-cols-4 gap-5">
+            <div>
+                {/* From Flowbite, data cards */}
+                <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">$ 1,000</h5>
+                    <p className="font-normal text-gray-700 dark:text-gray-400">Income</p>
+                </a>
+            </div>
+            <div>
+                <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">$ 300</h5>
+                    <p className="font-normal text-gray-700 dark:text-gray-400">Budget Spent</p>
+                </a>
+            </div>
+            <div>
+                <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">$ 100</h5>
+                    <p className="font-normal text-gray-700 dark:text-gray-400">Budget Savings</p>
+                </a>
+            </div>
+            <div>
+                <a href="#" className="h-36 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">$ 400</h5>
+                    {/* Flowbite Progress Bar */}
+                    <div className="flex justify-between mb-1">
+                        <span className="text-base font-normal text-gray-700 dark:text-gray-400">Total Savings</span>
+                        <span className="text-sm font-medium text-darkblue dark:text-white">80%</span>
+                    </div>
+                    <div className="w-full bg-lightblue rounded-full h-2.5 dark:bg-gray-700">
+                        <div className="bg-darkblue h-2.5 rounded-full w-4/5"></div>
+                    </div>
+                    <p className="mt-1 text-right italic font-normal text-gray-400 dark:text-gray-400">$100 to goal!</p>
+
+                </a>
+            </div> 
+        </div><div className="px-8 py-0 w-full overflow-x-auto">
+                <div className="mb-4 text-2xl font-semibold text-gray-800">Recent Activity</div>
+                <div className="grid grid-cols-4">
+                    <table className="w-full col-span-3 border-collapse table-auto bg-white rounded-lg shadow">
+                        <thead>
+                            <tr>
+                                <th className="border p-3 bg-gray-100 text-left">Date</th>
+                                <th className="border p-3 bg-gray-100 text-left">Category</th>
+                                <th className="border p-3 bg-gray-100 text-right">Amount</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {transactions.map((transaction, index) => (
+                                <tr
+                                    key={index}
+                                    className="transition-colors hover:bg-gray-50"
+                                >
+                                    <td className="border p-3">{transaction.date}</td>
+                                    <td className="border p-3">{transaction.description}</td>
+                                    <td className="border p-3 text-green-600 font-semibold text-right">
+                                        ${transaction.amount.toFixed(2)}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                    <button className="ml-4 px-4 py-2 rounded-md bg-emerald-200 text-white hover:bg-emerald-300">
+                        Add Transaction
+                    </button>
+                </div>
+            </div></>
   );
 }
 
