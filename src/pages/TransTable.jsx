84b6de160e3784a45db280b5e77cc31b5f023f05
Siema Alam <siemaalam@gmail.com>
2023-10-15 19:04:01 -0400

    Update styling and add new transaction

--- src/pages/TransTable.jsx
+++ src/pages/TransTable.jsx
@@ -5,13 +5,14 @@ import { useAuth } from "../context/AuthContext";
 const transactions = [
     { date: '2023-10-14', description: 'Utilities', amount: 50.0 },
     { date: '2023-10-15', description: 'Clothing', amount: 120.0 },
-    { date: '2023-10-13', description: 'Restaurant', amount: 50.0 },
+    { date: '2023-10-13', description: 'Restaurant', amount: 15.0 },
+    { date: '2023-10-12', description: 'Restaurant', amount: 15.0 },
     // Add more transactions
-  ];
-  
-  // Sort the transactions by date in ascending order
-  transactions.sort((a, b) => new Date(b.date) - new Date(a.date));
-  
+];
+
+// Sort the transactions by date in ascending order
+transactions.sort((a, b) => new Date(b.date) - new Date(a.date));
+
 
 const TransTable = () => {
     const { currentUser, logout } = useAuth();
@@ -24,29 +25,29 @@ const TransTable = () => {
         }
     };
     return (
-        <><div className="px-8 py-6 grid grid-cols-4 gap-5">
+        <><div className="px-8 pt-8 grid grid-cols-4 gap-5">
             <div>
                 {/* From Flowbite, data cards */}
                 <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">$ 1,000</h5>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-darkblue dark:text-white">$ 1,000</h5>
                     <p className="font-normal text-gray-700 dark:text-gray-400">Income</p>
                 </a>
             </div>
             <div>
                 <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">$ 300</h5>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-darkblue dark:text-white">$ 300</h5>
                     <p className="font-normal text-gray-700 dark:text-gray-400">Budget Spent</p>
                 </a>
             </div>
             <div>
                 <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">$ 100</h5>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-darkblue dark:text-white">$ 100</h5>
                     <p className="font-normal text-gray-700 dark:text-gray-400">Budget Savings</p>
                 </a>
             </div>
             <div>
                 <a href="#" className="h-36 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">$ 400</h5>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-darkblue dark:text-white">$ 400</h5>
                     {/* Flowbite Progress Bar */}
                     <div className="flex justify-between mb-1">
                         <span className="text-base font-normal text-gray-700 dark:text-gray-400">Total Savings</span>
@@ -58,10 +59,10 @@ const TransTable = () => {
                     <p className="mt-1 text-right italic font-normal text-gray-400 dark:text-gray-400">$100 to goal!</p>
 
                 </a>
-            </div> 
+            </div>
         </div><div className="px-8 py-0 w-full overflow-x-auto">
-                <div className="mb-4 text-2xl font-semibold text-gray-800">Recent Activity</div>
-                <div className="grid grid-cols-4">
+                <div className="mb-4 text-2xl font-semibold text-darkblue">Recent Activity</div>
+                <div className="grid grid-cols-4 gap-5">
                     <table className="w-full col-span-3 border-collapse table-auto bg-white rounded-lg shadow">
                         <thead>
                             <tr>
@@ -78,19 +79,19 @@ const TransTable = () => {
                                 >
                                     <td className="border p-3">{transaction.date}</td>
                                     <td className="border p-3">{transaction.description}</td>
-                                    <td className="border p-3 text-green-600 font-semibold text-right">
+                                    <td className="border p-3 text-green font-semibold text-right">
                                         ${transaction.amount.toFixed(2)}
                                     </td>
                                 </tr>
                             ))}
                         </tbody>
                     </table>
-                    <button className="ml-4 px-4 py-2 rounded-md bg-emerald-200 text-white hover:bg-emerald-300">
+                    <button className="text-base h-1/4 py-2 font-normal rounded-lg bg-darkblue text-lightblue hover:bg-black">
                         Add Transaction
                     </button>
                 </div>
             </div></>
-  );
+    );
 }
 
 export default TransTable;
