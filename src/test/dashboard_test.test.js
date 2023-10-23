538a206f8a81f7c31a7aed60db2ec97193ced1de
Siema Alam <siemaalam@gmail.com>
2023-10-23 16:59:21 -0400

    add unit test for early dashboard displays

---
+++ src/test/dashboard_test.test.js
@@ -0,0 +1,13 @@
+const displayTotalSpent = require('./dashboard_test')
+
+test('Shows the correct total spent', () => {
+  expect(displayTotalSpent(1000, 300, 100, 400)).toBe(300);
+});
+
+test('Shows the correct error message', () => {
+  expect(displayTotalSpent(1000, '', 100, 400)).toBe('Error in database. Spent should be an integer.');
+});
+
+test('Shows the correct error message', () => {
+  expect(displayTotalSpent(1000, '10', 100, 400)).toBe('Error in database. Spent should be an integer.');
+});
