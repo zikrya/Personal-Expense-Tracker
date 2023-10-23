eb1f2490c13325fc699650bb2a2504d3c1ab1ca4
Siema Alam <siemaalam@gmail.com>
2023-10-23 17:08:49 -0400

    update unit tests

--- src/test/dashboard_test.js
+++ src/test/dashboard_test.js
@@ -1,9 +1,3 @@
-// function sum(a, b) {
-//     return a + b;
-//   }
-// module.exports = sum;
-
-// this function will add up the expenses listed and sum the total, then subtract it from the budget
 class User_Dashboard {
     constructor(income, budget_spent, budget_savings, total_savings) {
         this.income = income;
@@ -21,9 +15,9 @@ function displayTotalSpent(i, bs, bsv, ts) {
     // create a new user and display total spent
     const first_user = new User_Dashboard(i, bs, bsv, ts);
     if(Number.isInteger(first_user.budget_spent)) { // check if it's a number
-        return first_user.displaySpent();
+        return first_user.displaySpent(); // return the number
     } else {
-        return 'Error in database. Spent should be an integer.'
+        return 'Error in database. Spent should be an integer.' // error message
     }
 }
 
