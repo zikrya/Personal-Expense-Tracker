2d37e8d12535493880a678f1b4ed8f0aec4b94d6
Siema Alam <siemaalam@gmail.com>
2023-10-30 02:11:46 -0400

    update unit test

--- src/test/unit/dashboard_test.test.js
+++ src/test/unit/dashboard_test.test.js
@@ -1,14 +1,16 @@
-// unit test
-const displayTotalSpent = require('./dashboard_test')
+// This is a unit test that takes in the created class (in the file this is just for context, 
+// not what we are testing) and tests the displayTotalSpent function. This function is just
+// checking whether the user was created and displayed information is a number or not.
+const returnTotalSpent = require('./dashboard_test')
 
-test('Shows the correct total spent', () => {
-  expect(displayTotalSpent(1000, 300, 100, 400)).toBe(300);
+test('Shows the correct total spent when input is a number', () => {
+  expect(returnTotalSpent(1000, 300, 100, 400)).toBe(300);
 });
 
-test('Shows the correct error message', () => {
-  expect(displayTotalSpent(1000, '', 100, 400)).toBe('Error in database. Spent should be an integer.');
+test('Shows the correct error message when total spent is empty', () => {
+  expect(returnTotalSpent(1000, '', 100, 400)).toBe('Error in database. Spent should be an integer.');
 });
 
-test('Shows the correct error message with string number', () => {
-  expect(displayTotalSpent(1000, '10', 100, 400)).toBe('Error in database. Spent should be an integer.');
+test('Shows the correct error message when total spent is a string number', () => {
+  expect(returnTotalSpent(1000, '10', 100, 400)).toBe('Error in database. Spent should be an integer.');
 });
