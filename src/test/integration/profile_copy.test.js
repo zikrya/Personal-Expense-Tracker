0b9320b46a1887572e0532ec94154c23809439c1
Siema Alam <siemaalam@gmail.com>
2023-11-01 00:51:18 -0400

    add e2e test

--- src/test/integration/profile_copy.test.js
+++ src/test/integration/profile_copy.test.js
@@ -7,9 +7,9 @@ import { getProfile  } from './profile_copy';
 // the UID below actually exists in our firebase database "users" and getProfile checks if it can
 test('Shows that the function can get existing data from the database (UID exists)', async () => {
   expect(getProfile("CrlJbP4uftdQ47XNoAtHgI4alDH3")).toBe(0);
-});
+}, 15000);
 
 // the UID below is random and does not exist in our firebase, so this code tests "errors"
 test('Shows that the function can determine that data does not exist for a non-existent UID', async () => {
   expect(getProfile("nonExistentUID")).not.toBe(1);
-});
+}, 15000);
