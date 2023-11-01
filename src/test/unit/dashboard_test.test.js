ba145c04b7cad771cc070fef16bfaa932d049149
Siema <53492402+aappacode@users.noreply.github.com>
2023-11-01 01:35:55 -0400

    Add comments to dashboard_test.test.js

--- src/test/unit/dashboard_test.test.js
+++ src/test/unit/dashboard_test.test.js
@@ -5,11 +5,12 @@
 
 import {returnTotalSpent} from './dashboard_test';
 
-
+// when the class has an actual number, i.e. 300, it should return that number
 test('Shows the correct total spent when input is a number', () => {
   expect(returnTotalSpent(1000, 300, 100, 400)).toBe(300);
 });
 
+// below are cases where an error message should be returned since the value in the function is not a number.
 test('Shows the correct error message when total spent is empty', () => {
   expect(returnTotalSpent(1000, '', 100, 400)).toBe('Error in database. Spent should be an integer.');
 });
