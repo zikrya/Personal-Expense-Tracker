5e39e7edae84a69e33bdae776a2717873dcdf4d1
Siema Alam <siemaalam@gmail.com>
2023-10-31 22:30:17 -0400

    Add integration test and attempt of e2e test

--- src/test/integration/profile_copy.test.js
+++ src/test/integration/profile_copy.test.js
@@ -2,9 +2,14 @@
 // import { firestore } from '../utils/firebase-config';
 // import { doc, getDoc } from 'firebase/firestore';
 // const { getProfile } = require('./profile_copy');
-import { getProfile } from './profile_copy';
+import { getProfile  } from './profile_copy';
 
-test('Shows the correct total spent when input is a number', async () => {
-  getProfile("CrlJbP4uftdQ47XNoAtHgI4alDH3");
+// the UID below actually exists in our firebase database "users" and getProfile checks if it can
+test('Shows that the function can get existing data from the database (UID exists)', async () => {
   expect(getProfile("CrlJbP4uftdQ47XNoAtHgI4alDH3")).toBe(0);
-}, 600000);
+});
+
+// the UID below is random and does not exist in our firebase, so this code tests "errors"
+test('Shows that the function can determine that data does not exist for a non-existent UID', async () => {
+  expect(getProfile("nonExistentUID")).toBe(1);
+});
