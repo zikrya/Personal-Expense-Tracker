ca6fe636627469ecaa722e2d43a368367dee4d1d
Siema Alam <siemaalam@gmail.com>
2023-10-31 22:30:17 -0400

    Add integration test and attempt of e2e test

--- src/test/integration/profile_copy.js
+++ src/test/integration/profile_copy.js
@@ -2,22 +2,22 @@ import { firestore } from '../../utils/firebase-config';
 import { doc, getDoc } from 'firebase/firestore';
 
 
+
 export function getProfile(UID) {
     // Fetching the profile data from Firestore when the component mounts
-    let boolReturned = 0; 
+    let boolReturned = 0;
     const fetchData = async () => {
         const docRef = doc(firestore, "users", UID);
         const docSnap = await getDoc(docRef);
 
         if (docSnap.exists()) {
-            //console.log(docSnap.data());
+            // console.log(docSnap.data());
             boolReturned = 0;
         } else {
-           // console.log("No such document!");
+            //console.log("No such document!");
             boolReturned = 1;
         }
     };
     fetchData();
     return boolReturned;
 }
-
