a4318d9b5b64e18b72188cc00f0f679d7968db1c
Siema Alam <siemaalam@gmail.com>
2023-10-31 23:40:47 -0400

    update comments

--- src/test/integration/profile_copy.js
+++ src/test/integration/profile_copy.js
@@ -1,7 +1,8 @@
 import { firestore } from '../../utils/firebase-config';
 import { doc, getDoc } from 'firebase/firestore';
-
-
+// due to this code requiring an async function, I had to install some babel/updates based on these instructions:
+// https://stackoverflow.com/questions/58613492/how-to-resolve-cannot-use-import-statement-outside-a-module-from-jest-when-run
+// which had to make me remove "type": "module" from my package.json, and switch the way i'm importing and exporting functions.
 
 export function getProfile(UID) {
     // Fetching the profile data from Firestore when the component mounts
