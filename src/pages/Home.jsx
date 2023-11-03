289c4598594dcccd15ad40bd248367ee93517e9b
gaoqiangzhou <gaoqiangzhouer@gmail.com>
2023-11-02 20:21:27 -0400

    added guide to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import HomeIntro from "../components/HomeIntro";
+import HomeGuide from "../components/HomeGuide";
 
 const Home = () => {
 
@@ -6,6 +7,7 @@ const Home = () => {
     return (
         <div>
             <HomeIntro/>
+            <HomeGuide/>
         </div>
     );
 }
