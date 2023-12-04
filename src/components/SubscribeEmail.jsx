f248f5d711137ea2a0ba5d2bc128da73bba26a55
Siema <siemaalam@gmail.com>
2023-12-03 23:13:05 -0500

    working email area

--- src/components/SubscribeEmail.jsx
+++ src/components/SubscribeEmail.jsx
@@ -1,34 +1,42 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 
+
+
 const Subscribe = () => {
+
   const form = useRef();
+
+  const templateParams = {
+    recipient: 'your.wisewallet@gmail.com',
+    to_name: 'Test Account',
+    message: 'This is a test email sent using EmailJS!'
+  };
 
   const sendEmail = (e) => {
     e.preventDefault();
-    var templateParams = {
-        to_name: 'Test',
-        recipient: 'your.wisewallet@gmail.com'
-    };
-    emailjs.send('service_vcya2vm', 'welcome_1ywtamz', templateParams)
-      .then(() => {
-          alert("Success");
-      }, () => {
-          alert("Failed");
+    emailjs.send('service_vcya2vm', 'template_57i8kep', templateParams, 'xZBCwlq2NxOnENizZ')
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+        alert("Success. Message sent!")
+
+      }, (err) => {
+        console.log('FAILED...', err);
+        alert("Failure. Message did not send!")
+
       });
   };
 
   return (
-    <div>
-    <h1>Subscribe</h1>
     <form ref={form} onSubmit={sendEmail}>
       <label>Name</label>
       <input type="text" name="user_name" />
-      <label>Email</label>
+      <label>Your Email</label>
       <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
       <input type="submit" value="Send" />
     </form>
-    </div>
   );
 };
 
